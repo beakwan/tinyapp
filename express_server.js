018335ecbb63bd927ef971bc4b560729e1050f9a
Beatrice Kwan <beatrice.c.kwan@gmail.com>
2021-07-06 17:29:34 +0000

    addition of redirects from short url to long url

--- express_server.js
+++ express_server.js
@@ -2,12 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
-app.set("view engine", "ejs");
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
-const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
+app.set("view engine", "ejs");
 
 //Function to generate random 6 character string
 const generateRandomString = function() {
@@ -15,41 +13,58 @@ const generateRandomString = function() {
   return string;
 };
 
+//Current hardcoded database
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
 
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
 
-app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+//Says hello on the homepage
+app.get("/", (req, res) => {
+  res.send("Hello!");
 });
 
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
-});
-
+//Gets all of the urls
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+//Creates new urls
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+//Shows long url from short url
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL};
   res.render("urls_show", templateVars);
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
+//Redirects to long url from short url
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
 
+//Generates new tiny url for long url input and redirects to show the new tiny url
+app.post("/urls", (req, res) => {
+  const shortCode = generateRandomString();
+  urlDatabase[shortCode] = req.body.longURL;
+  console.log(req.body);  
+  res.redirect(`urls/${shortCode}`);         
+});
+
+
+//Set up listen, and log to ensure it is on the correct port
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+//
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// });
+
