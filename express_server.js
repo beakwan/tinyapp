8d24174e6fe3608a6ce94721c20dc52da8db3959
Beatrice Kwan <beatrice.c.kwan@gmail.com>
2021-07-07 20:17:01 +0000

    update of registration process including errors and addition of email shown in header

--- express_server.js
+++ express_server.js
@@ -10,68 +10,53 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
+
+
+//Current hardcoded databases
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+const users = {
+  user1: {
+    id: "user1",
+    email: "email@email.com",
+    password: "hello"
+  }
+};
+
+
+
+//HELPER Functions
 //Function to generate random 6 character string
 const generateRandomString = function() {
   string = Math.random().toString(36).substring(2, 8);
   return string;
 };
 
-//Current hardcoded database
-const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+//Function to loop through users database to find correct id
+const findUserById = function(userId, userDB) {
+  for (let user in userDB) {
+    if (user === userId) {
+      return userDB[user];
+    } 
+  }
+};
+
+//Function to lookup a user email
+const findUserEmail = function(email, userDB) {
+  for (let user in userDB) {
+    if (userDB[user].email === email) {
+      return userDB[user].email;
+    } else {
+      return null;
+    }
+  }
 };
 
 
-
-//All GET requests
-//Says hello on the homepage
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
-//Gets all of the urls
-app.get("/urls", (req, res) => {
-  const templateVars = { 
-    urls: urlDatabase,
-    username: req.cookies["username"]
-   };
-  res.render("urls_index", templateVars);
-});
-
-//Creates new urls
-app.get("/urls/new", (req, res) => {
-  const templateVars = {
-    username: req.cookies["username"]
-  };
-  res.render("urls_new", templateVars);
-});
-
-//Shows long url from short url
-app.get("/urls/:shortURL", (req, res) => {
-  templateVars = {
-    shortURL: req.params.shortURL,
-    longURL: urlDatabase[req.params.shortURL],
-    username: req.cookies["username"]
-  };
-  res.render("urls_show", templateVars);
-});
-
-//Redirects to long url from short url
-app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL];
-  res.redirect(longURL);
-});
-
-//Shows a page to register
-app.get("/register", (req, res) => {
-  const templateVars = {
-    username: req.cookies["username"]
-  };
-  res.render("register", templateVars);
-});
-
-
+console.log(findUserEmail("email@email.com", users));
 
 
 //All POST requests
@@ -79,7 +64,6 @@ app.get("/register", (req, res) => {
 app.post("/urls", (req, res) => {
   const shortCode = generateRandomString();
   urlDatabase[shortCode] = req.body.longURL;
-  console.log(req.body);  
   res.redirect(`urls/${shortCode}`);         
 });
 
@@ -99,7 +83,7 @@ app.post("/u/:shortURL", (req, res) => {
 
 //Set login username to cookie and redirect back to urls page
 app.post("/login", (req, res) => {
-  res.cookie("username", req.body.username);
+  //res.cookie("username", req.body.username);
   res.redirect("/urls");
 });
 
@@ -108,6 +92,85 @@ app.post("/logout", (req, res) => {
   res.clearCookie("username");
   res.redirect("/urls");
 });
+
+//Register new user and redirect to urls page
+app.post("/register", (req, res) => {
+  if (!req.body.email || !req.body.password) {
+    res.send("400! Please enter a valid email and password");
+  } else if (findUserEmail(req.body.email, users)) {
+    res.send("400! Please enter a new email address");
+  } else {
+    const id = generateRandomString();
+    users[id] = {
+      id: id,
+      email: req.body.email,
+      password: req.body.password
+    };
+    res.cookie("user_id", id);
+    res.redirect("/urls");
+  }
+});
+
+
+
+
+
+
+//All GET requests
+//Says hello on the homepage
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+//Gets all of the urls
+app.get("/urls", (req, res) => {
+  const user = findUserById(req.cookies.user_id, users);
+  const templateVars = { 
+    urls: urlDatabase,
+    user
+   };
+  res.render("urls_index", templateVars);
+});
+
+//Creates new urls
+app.get("/urls/new", (req, res) => {
+  const user = findUserById(req.cookies.user_id, users);
+  const templateVars = {
+    user
+  };
+  res.render("urls_new", templateVars);
+});
+
+//Shows long url from short url
+app.get("/urls/:shortURL", (req, res) => {
+  const user = findUserById(req.cookies.user_id, users);
+  templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+    user
+  };
+  res.render("urls_show", templateVars);
+});
+
+//Redirects to long url from short url
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
+//Shows a page to register
+app.get("/register", (req, res) => {
+  const user = findUserById(req.cookies.user_id, users);
+  const templateVars = {
+    user
+  };
+  res.render("register", templateVars);
+});
+
+
+
+
+
 
 
 
